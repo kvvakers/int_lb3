abff1fdba1413bec62e19e927b28ccb92d02df78
kvvakers <56801548+kvvakers@users.noreply.github.com>
2024-12-15 13:30:17 +0200

    Add files via upload
    add save table

--- javascript.js
+++ javascript.js
@@ -1,10 +1,11 @@
 let intervalId;
 const tableBody = document.getElementById('data-table').querySelector('tbody');
-
+let tableData = [];
 function getRandomData() {
     const randomNumber = Math.floor(Math.random() * 1000);
     const randomText = Math.random().toString(36).substring(7);
     const currentDate = new Date().toLocaleString();
+    tableData.push({ randomNumber, randomText, currentDate });
     return { randomNumber, randomText, currentDate };
 }
 
@@ -42,4 +43,15 @@ function stopGeneration() {
 function clearTable() {
     clearInterval(intervalId);
     tableBody.innerHTML = '';
+}
+
+function saveTable() {
+    let string_table_data = JSON.stringify(tableData);
+
+    const blob = new Blob([string_table_data], {type: "appication/json"});
+    const save_a = document.createElement('a');
+    save_a.href = URL.createObjectURL(blob);
+    save_a.download = "data.json";
+    save_a.innerText = "save";
+    document.body.appendChild(save_a);
 }
